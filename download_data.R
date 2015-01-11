868850fe22be2c9a7a261642ce52bec8593f2ed2
Antonio Fernandez <antonio.fernandez@bq.com>
2015-01-11 17:51:48 +0100

    rereading the exercise all plot files must be able to reproduce the plot in one step

--- download_data.R
+++
@@ -1,9 +0,0 @@
-# Step 0. Download data.
-
-fileName <- 'dataset.zip'
-if (!file.exists(fileName)) {
-  print("Downloading data...")
-  url <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
-  download.file(url, fileName, method = 'curl')
-  unzip(fileName)
-}
