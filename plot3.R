565c875ad8b17e8d7bb4adb3d9c6f0df841c4ce6
Antonio Fernandez <antonio.fernandez@bq.com>
2015-01-11 18:16:19 +0100

    plot3

---
+++ plot3.R
@@ -0,0 +1,62 @@
+# Download data?
+fileName <- 'dataset.zip'
+if (!file.exists(fileName)) {
+  print("Downloading data...")
+  url <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
+  download.file(url, fileName, method = 'curl')
+  unzip(fileName)
+}
+
+# library lubridate
+library(lubridate)
+
+# Read data
+col_classes <- c('character','character','numeric','numeric','numeric','numeric','numeric','numeric')
+power_data <- read.table('household_power_consumption.txt',
+                         header=TRUE,
+                         colClasses=col_classes,
+                         sep=';',
+                         na.strings='?') 
+# Subsetting data
+power_data_subset <- power_data[power_data$Date=='1/2/2007' | power_data$Date=='2/2/2007',]
+
+# tidy up data
+cols<-c('Date',
+        'Time',
+        'GlobalActivePower',
+        'GlobalReactivePower',
+        'Voltage',
+        'GlobalIntensity',
+        'SubMetering1',
+        'SubMetering2',
+        'SubMetering3')
+colnames(power_data_subset)<-cols
+
+png(filename='plot3.png',
+    width=480,
+    height=480,
+    units='px')
+
+lines_colors <- c('black','red','blue')
+lines_labels <- c('Sub_metering_1','Sub_metering_2','Sub_metering_3')
+
+plot(dmy(power_data_subset$Date)+hms(power_data_subset$Time),
+     power_data_subset$SubMetering1,
+     type='l',
+     col=lines_colors[1],
+     xlab='',
+     ylab='Energy sub metering')
+lines(dmy(power_data_subset$Date)+hms(power_data_subset$Time),
+      power_data_subset$SubMetering2,
+      col=lines_colors[2])
+lines(dmy(power_data_subset$Date)+hms(power_data_subset$Time),
+      power_data_subset$SubMetering3,
+      col=lines_colors[3])
+legend('topright',
+       legend=lines_labels,
+       col=lines_colors,
+       lty='solid')
+
+# dev.off
+x<-dev.off()
+
